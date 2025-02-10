afe800864141522278bc82bc1c9f8c7785564f89
xiangshangzhi <xiangshangzhi@163.com>
2025-02-10 10:24:15 +0800

    feat: readme 文档

--- src/demo.py
+++ src/demo.py
@@ -1,3 +1,13 @@
-print('hello, world')
+# print('hello, world')
 
-print('hello, world xiangshangzhi')
+# print('hello, world xiangshangzhi')
+
+
+list = [1, 2, 3, 4, 5] 
+list[1] = 'hello'
+# print(list)
+
+tuple = ('hello', 'world')
+ctstr = ('-').join(tuple)
+print(ctstr)
+
