ca4cf45e7ba311ff5ee7c0b4b3ca50531582c36a
xiangshangzhi <xiangshangzhi@163.com>
2025-03-18 11:22:49 +0800

    feat: 更新requirement文件

--- app/api/hello/services/auth_service.py
+++
@@ -1,8 +0,0 @@
-from ..models.user import UserModel
-from app.utils.jwt_utils import JWTUtils
-
-def login_user(email, password):
-    user = UserModel.query.filter_by(email=email).first()
-    if user and user.check_password(password):
-        return JWTUtils.reate_tokens(user.id)
-    return None
